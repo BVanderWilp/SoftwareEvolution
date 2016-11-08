356e6f24807af73eef1d3d91287a85782dff6629
bvanderwilp <bvanderwilp@gmail.com>
2016-11-08 17:38:23 -0600

    comments for assignment3

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,8 @@
 public class Hello
 {
+	/** run jump
+	* IllegalArgumentException
+	*/
 	static void main(String...args)
 	{
 		System.out.println("Hello World");
--- src2/Hello.java
+++ src2/Hello.java
@@ -1,5 +1,8 @@
 public class Hello
 {
+	/** fly sing work
+	* IllegalArgumentException IllegalArgumentException
+	*/
 	static void main(String...args)
 	{
 		System.out.println("Hello World");
